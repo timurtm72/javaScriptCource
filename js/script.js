8774b9bb4a40fe3bf4c47941bea3299b80639912
timurtm72 <timur.sultanov1972@gmail.com>
2023-04-18 19:49:29 +0300

    lesson 23

--- js/script.js
+++ js/script.js
@@ -1,34 +1,8 @@
-"use strict";
-const numberOfFilms = +prompt('Сколько фильмов Вы уже просмотрели?','');
-const personalMovieDB = {
-    count:numberOfFilms,
-    movies:{},
-    actors:{},
-    genres:[],
-    private:false
-};
-for(let i = 0;i < 2; i++){
-    const   a = prompt('Один из послдених просмотренных фильмов?',''),
-    b = prompt('На сколько оцение его?','')
-
-    if(a != nuul && b != null && a != '' && b != '' && a.length < 50){
-        personalMovieDB.movies[a] = b;
-        console.log('Done!');
-    }else{
-        console.log('Error');
-        i--;
-    }
+let num = 20;
+function showFirstMessage(message) {
+  console.log(`Hello ${message}`);
+  num = 10;
+  console.log(num);
 }
-if(personalMovieDB.count < 10){
-    console.log('Просмотренно мало фильмов');
-}else if(personalMovieDB.count >= 10 && personalMovieDB.count > 30){
-    console.log('Вы классический зритель.');
-}else if(personalMovieDB.count >= 30){
-    console.log('Вы киноман.');
-}else{
-    console.log('Произошла ошибка.');
-}
-console.log(personalMovieDB);
-
-
-   
+showFirstMessage('Petr');
+console.log(num);
