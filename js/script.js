29e1532ad456217d09c9baab4e059bad63a50fbe
timurtm72 <timur.sultanov1972@gmail.com>
2023-04-18 13:37:46 +0300

    lesson 17

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
+const numberOfFilms = +prompt('Сколько фильмов Вы уже просмотрели?','');
+const personalMovieDB = {
+    count:numberOfFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    private:false
+};
+const   a = prompt('Один из послдених просмотренных фильмов?',''),
+        b = prompt('На сколько оцение его?','')
+        c = prompt('Один из послдених просмотренных фильмов?',''),
+        d = prompt('На сколько оцение его?','');
 
+        personalMovieDB.movies[a] = b;
+        personalMovieDB.movies[c] = d;
+
+        console.log(personalMovieDB);
